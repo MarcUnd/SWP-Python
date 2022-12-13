24746c546597a6b3efd4938d1f473c283bb73470
MarcUnd <und.marc@gmail.com>
2022-12-13 12:11:01 +0100

    Update OOP.py

--- OOP-Übung/OOP.py
+++ OOP-Übung/OOP.py
@@ -27,14 +27,16 @@ class firma():
         return ab.name
     
     def getGenderRatio(self):
-        dic = {'mann': 0, 'frau': 0}
+        dic = {'mann': 0, 'frau': 0, 'other': 0}
         ges = self.getMitarbeiterAnz()
         for a in self.abteilungen:
             for p in a.mitarbeiter:
                 if p.geschlecht == 'mann':
                     dic['mann'] += round(1/ges, 4)
-                else:
+                elif p.geschlecht == 'frau':
                     dic['frau'] += round(1/ges, 4)
+                else:
+                    dic['other'] += round(1/ges, 4)
         return dic
     
     
@@ -57,13 +59,13 @@ class person():
 
 class mitarbeiter(person):
     
-    def __init__(self, vname, nname, alter, geschlecht, abteilung):
+    def __init__(self, vname, nname, alter, geschlecht, abteilung=None):
         super().__init__(vname, nname, alter, geschlecht)
         self.abteilung = abteilung
 
 class abteilungsleiter(mitarbeiter):
     
-    def __init__(self, vname, nname, alter, geschlecht, abteilung):
+    def __init__(self, vname, nname, alter, geschlecht, abteilung=None):
         super().__init__(vname, nname, alter, geschlecht, abteilung)
 
 
@@ -71,13 +73,13 @@ class abteilungsleiter(mitarbeiter):
 
 if __name__ == '__main__':
     f1 = firma('Tollste Firma ever', [])
-    m1 = mitarbeiter('Hans', 'Peter', 68, 'mann', None)
-    m2 = mitarbeiter('Birgitt', 'Brigitte', 15, 'frau', None)
-    m3 = mitarbeiter('Marlene', 'Brigitte', 15, 'frau', None)
-    m4 = mitarbeiter('Hallo', 'Brigitte', 15, 'mann', None)
-    m5 = mitarbeiter('Pedda', 'Pedda', 15, 'mann', None)
-    al1 = abteilungsleiter('Peter', 'Hans', 50, 'mann', None)
-    al2 = abteilungsleiter('Favi', 'Favi', 500, 'mann', None)
+    m1 = mitarbeiter('Hans', 'Peter', 68, 'mann')
+    m2 = mitarbeiter('Birgitt', 'Brigitte', 15, 'frau')
+    m3 = mitarbeiter('Marlene', 'Brigitte', 15, 'frau')
+    m4 = mitarbeiter('Hallo', 'Brigitte', 15, 'apache kampfhelikopter')
+    m5 = mitarbeiter('Pedda', 'Pedda', 15, 'mann')
+    al1 = abteilungsleiter('Peter', 'Hans', 50, 'mann')
+    al2 = abteilungsleiter('Favi', 'Favi', 500, 'mann')
     a1 = abteilung('IT', [m1, al1, m2], al1, f1)
     a2 = abteilung('HR', [m3, m4, al2, m5], al2, f1)
     
